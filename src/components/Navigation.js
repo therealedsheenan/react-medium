4165fd77e8cfc9658ec8032eb1c207611a7f09d3
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-16 18:22:09 +0900

    add basic navigation for react-medium menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Menu, Container } from 'semantic-ui-react';
 
 const Navigation = () => {
   return (
-    <Menu pointing secondary>
-      <Menu.Item
-        name="React-Medium"
-        active={false}
-        content="React-Medium"
-        onClick={() => {}}
-      />
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item active={false}>
+          <Link to="/">React Medium</Link>
+        </Menu.Item>
+      </Container>
     </Menu>
   );
 };
