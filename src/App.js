1ef5f9409604abd0d38e3a108b6a56672cc514c9
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-16 18:23:00 +0900

    basic app structure and styling configurations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import NotFound from './components/NotFound';
+import Footer from './components/Footer';
 import Home from './pages/Home/';
+import Post from './pages/Post/';
 import './App.css';
 
 import 'semantic-ui-css/semantic.min.css';
@@ -13,8 +15,10 @@ const App = () => (
     <Navigation />
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/post/:postId" component={Post} />
       <Route component={NotFound} />
     </Switch>
+    <Footer />
   </main>
 );
 
