21bf8f8c5ad0705c568e80fb33995f96d7f9ffe1
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-16 23:04:24 +0900

    add authentication function on navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,62 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import history from '../history';
 
-const Navigation = () => {
+import auth from '../services/authentication';
+import userActions from '../store/user/actions';
+
+const Navigation = ({ logoutUser, user }) => {
+  useEffect(() => {
+    auth.isAuthenticated();
+  }, user.isLoading);
   return (
     <Menu fixed="top" inverted>
       <Container>
         <Menu.Item active={false}>
           <Link to="/">React Medium</Link>
         </Menu.Item>
-        <Menu.Item active={false} position="right">
-          <Button inverted>
-            <Link to="/user/login">Login </Link>
-          </Button>
-          <Button inverted style={{ marginLeft: '0.5em' }}>
-            <Link to="/user/new"> Sign Up</Link>
-          </Button>
-        </Menu.Item>
+        {!auth.isAuthenticated() ? (
+          <Menu.Item active={false} position="right">
+            <Button inverted>
+              <Link to="/user/login">Login </Link>
+            </Button>
+            <Button inverted style={{ marginLeft: '0.5em' }}>
+              <Link to="/user/new"> Sign Up</Link>
+            </Button>
+          </Menu.Item>
+        ) : (
+          <Menu.Item active={false} position="right">
+            <Button inverted onClick={logoutUser}>
+              Logout
+            </Button>
+          </Menu.Item>
+        )}
       </Container>
     </Menu>
   );
 };
 
-export default Navigation;
+Navigation.defaultProps = {
+  user: {}
+};
+
+Navigation.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  user: PropTypes.object
+};
+
+const mapStateToProps = ({ user }) => ({ user });
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logoutUser: () => dispatch(userActions.logoutUser(history))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Navigation);
