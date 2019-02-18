abeb3d65c8aa8fd20a2ae5d47e9f123a4b2b766a
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-18 22:25:18 +0900

    implement renaming of file changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
-import './index.css';
+import './assets/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
 import postReducers from './posts/reducers';
 import commentReducers from './comments/reducers';
 import userReducers from './user/reducers';
+import clapReducers from './claps/reducers';
 
 const reducer = {
   postsList: postReducers.postsListReducer,
   postItem: postReducers.postItemReducer,
   commentsList: commentReducers.commentsListReducer,
-  user: userReducers.userReducer
+  user: userReducers.userReducer,
+  claps: clapReducers.clapsReducer
 };
 
 const middleware = [...getDefaultMiddleware()];
